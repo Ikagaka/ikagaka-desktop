a85e339ee7dbe4257f291431c55ee2fb1da9045d
Narazaka <info@narazaka.net>
2017-12-20 04:31:25 +0900

    開発用にlivereloadをいれる

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import * as url from "url";
 
+if (process.env.ELECTRON_RELOAD) {
+    // tslint:disable-next-line no-var-requires no-require-imports no-implicit-dependencies
+    require("electron-reload")(__dirname);
+}
+
 let mainWindow: BrowserWindow | undefined;
 
 app.on("ready", () => {
