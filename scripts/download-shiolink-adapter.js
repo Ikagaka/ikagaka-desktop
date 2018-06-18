e8c62c54acf83f95fd3c50658c3e274865b0234f
Narazaka <info@narazaka.net>
2018-06-19 00:14:27 +0900

    shiolink_adapter.exeのダウンロード先を変更

--- scripts/download-shiolink-adapter.js
+++ scripts/download-shiolink-adapter.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const JSZip = require("jszip");
 const fetch = require("node-fetch").default;
 const path = require("path");
 
@@ -8,10 +9,13 @@ if (fs.existsSync(exePath)) {
     process.exit();
 }
 
-fetch("https://github.com/Ikagaka/shiolink_adapter/raw/master/shiolink_adapter.exe").then((response) => {
+fetch("https://github.com/Ikagaka/shiolink_adapter/releases/download/1.0.0.1/shiolink_adapter.zip").then((response) => {
     if (!response.ok) throw new Error("cannot download");
     return response.buffer();
-}).then((file) => {
+})
+.then(JSZip.loadAsync)
+.then((zip) => zip.file("shiolink_adapter.exe").async("nodebuffer"))
+.then((file) => {
     fs.writeFileSync(exePath, file);
     console.warn("shiolink_adapter.exe downloaded");
 });
